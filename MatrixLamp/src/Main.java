7488a6311aa9daf9de87e87b9825bd56aad5e263
Tochi <tochi.caraballo@gmail.com>
2016-11-20 18:49:14 -0800

    In progress

--- MatrixLamp/src/Main.java
+++ MatrixLamp/src/Main.java
@@ -8,9 +8,9 @@ public class Main {
         Operations myOperation = new Operations();
         int array[][] = {
                 {0,0,0,0,0},
+                {1,1,1,1,0},
                 {0,0,0,0,0},
-                {0,0,0,0,0},
-                {0,0,0,0,0},
+                {0,1,1,1,1},
                 {0,0,0,0,0}
         };
 
@@ -20,19 +20,42 @@ public class Main {
         int lengthCols = array[0].length;
         System.out.println("Rows: " + lengthRows + " Columns: " + lengthCols);
 
-        for(int i = 0; i < lengthRows; i++){
-            for (int j = 0; j < lengthCols; j++){
-                System.out.print(array[i][j] + ", ");
-            }
-            System.out.println();
+        array = myOperation.deActivateAround(array,0,0, lengthCols,lengthRows);
+        array = myOperation.deActivateAround(array,4,4, lengthCols,lengthRows);
+/*
+        // check vertical line
+        if(myOperation.checkHorizontal(array, lengthCols, 3)){
+            System.out.println("It's on because other lamps");
+        }
+        else{
+            System.out.println("No sign. Lamp is off");
         }
 
+        // check horizontal line
+        if(myOperation.checkVertical(array, lengthRows, 0)){
+            System.out.println("It's on because other lamps");
+        }
+        else{
+            System.out.println("No sign. Lamp is off");
+        }
+*/
+        // checks adjacent on the way up
+        if(myOperation.checkAdjacentUp(array,2,2, lengthCols,lengthRows)){
+            System.out.println("It's on because other lamps");
+        }
+        else{
+            System.out.println("No sign. Lamp is off");
+        }
+
+        //myOperation.print2dArray(array, lengthRows, lengthCols);
+/*
         if(myOperation.inRange(0,4,lengthCols,lengthRows)){
             System.out.println("It's in the range");
         }
         else {
             System.out.println("Not in the range");
         }
+*/
     }
 
 }
--- MatrixLamp/src/Operations.java
+++ MatrixLamp/src/Operations.java
@@ -2,11 +2,74 @@
 public class Operations {
 
     public boolean inRange(int columnX, int rowY, int lengthCols, int lengthRows){
-        if(columnX < lengthCols && rowY < lengthRows){
+        if(columnX < lengthCols && rowY < lengthRows && columnX >= 0 && rowY >=0){
             return true;
         }
         else {
             return false;
         }
+    }
+
+    public void print2dArray(int [][] array, int lengthRows, int lengthCols){
+        for(int i = 0; i < lengthRows; i++){
+            for (int j = 0; j < lengthCols; j++){
+                System.out.print(array[i][j] + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int [][] deActivateAround(int [][] array, int columnX, int rowY, int lengthCols, int lengthRows){
+        int currentX = columnX - 1;
+        int currentY = rowY - 1;
+
+        int [][] myArray = new int[lengthCols][lengthRows];
+
+        for(int i= 0; i < lengthRows; i++){
+            for(int j = 0; j < lengthCols; j++){
+                myArray[i][j] = array[i][j];
+            }
+        }
+       // print2dArray(myArray, lengthCols,lengthRows);
+        for(int i= 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(inRange(currentX +j, currentX + i, lengthCols,lengthRows)){
+                    myArray[currentY + j][currentX + i] = 0;
+                }
+            }
+        }
+        return myArray;
+    }
+
+
+    public boolean checkHorizontal(int [][] array, int lengthCols, int fixYaxis){
+        for(int i = 0; i < lengthCols; i++){
+            if(array[i][fixYaxis] == 1)
+                return true;
+        }
+        return false;
+    }
+    public boolean checkVertical(int [][] array, int lengthRows, int fixXaxis){
+        for(int i = 0; i < lengthRows; i++){
+            if(array[fixXaxis][i] == 1)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean checkAdjacentUp(int [][] array, int currentX, int currentY, int lengthCols, int lengthRows){
+
+        int moveX = currentX;
+        int moveY = currentY;
+
+        while( inRange(moveX, moveY, lengthCols, lengthRows)){
+            //System.out.println(array[moveX][moveY]);
+            if(array[moveX][moveY] == 1){
+                return true;
+            }
+            moveX = moveX + 1;
+            moveY = moveY + 1;
+        }
+        return false;
     }
 }
